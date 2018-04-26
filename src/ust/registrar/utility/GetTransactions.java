c48c08a0521ab2a7c100e143f9cb4f2dde7982a7
roabert <trebavena@gmail.com>
2018-04-26 10:41:19 +0800

    More Count Methods

--- src/ust/registrar/utility/GetTransactions.java
+++ src/ust/registrar/utility/GetTransactions.java
@@ -20,6 +20,102 @@ public class GetTransactions implements DatabaseSQLs{
 		return count;
 	}
 	
+	public int CountShiftTransactions(Connection conn) {
+		int count = 0;
+		try {
+	  		PreparedStatement ps = conn.prepareStatement("SELECT COUNT(*) FROM transactions INNER JOIN student_shifter on student_id = student_shifter.studentid");
+	  		ResultSet getSY = ps.executeQuery();
+	  		while (getSY.next()){
+	  			count = getSY.getInt(1);
+	  		}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return count;
+	}
+
+	public int CountTransferTransactions(Connection conn) {
+		int count = 0;
+		try {
+	  		PreparedStatement ps = conn.prepareStatement("SELECT COUNT(*) FROM transactions INNER JOIN student_transfer on student_id = student_transfer.userid");
+	  		ResultSet getSY = ps.executeQuery();
+	  		while (getSY.next()){
+	  			count = getSY.getInt(1);
+	  		}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return count;
+	}
+
+	public int CountShifterSpecificTransactions(Connection conn, String userid) {
+		int count = 0;
+		try {
+	  		PreparedStatement ps = conn.prepareStatement("SELECT COUNT(*) FROM transactions INNER JOIN student_shifter on student_id = student_shifter.studentid WHERE user_role= ?");
+			ps.setString(1, userid);
+	  		ResultSet getSY = ps.executeQuery();
+	  		while (getSY.next()){
+	  			count = getSY.getInt(1);
+	  		}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return count;
+	}
+
+	public int CountTransferSpecificTransactions(Connection conn, String userid) {
+		int count = 0;
+		try {
+	  		PreparedStatement ps = conn.prepareStatement("SELECT COUNT(*) FROM transactions INNER JOIN student_transfer on student_id = student_transfer.userid WHERE user_role= ?");
+			ps.setString(1, userid);
+	  		ResultSet getSY = ps.executeQuery();
+	  		while (getSY.next()){
+	  			count = getSY.getInt(1);
+	  		}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return count;
+	}
+
+	public int CountShifterSuccessTransactions(Connection conn, String studentid, String userid) {
+		int count = 0;
+		try {
+	  		PreparedStatement ps = conn.prepareStatement("SELECT COUNT(*) FROM transactions INNER JOIN student_transfer on student_id = student_transfer.userid WHERE student_id = ? AND user_role= ?");
+			ps.setString(1, studentid);
+			ps.setString(2, userid);
+	  		ResultSet getSY = ps.executeQuery();
+	  		while (getSY.next()){
+	  			count = getSY.getInt(1);
+	  		}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return count;
+	}
+
+	public int CountTransferSuccessTransactions(Connection conn, String studentid, String userid) {
+		int count = 0;
+		try {
+	  		PreparedStatement ps = conn.prepareStatement("SELECT COUNT(*) FROM transactions INNER JOIN student_transfer on student_id = student_transfer.userid WHERE student_id = ? AND user_role= ?");
+			ps.setString(1, studentid);
+			ps.setString(2, userid);
+	  		ResultSet getSY = ps.executeQuery();
+	  		while (getSY.next()){
+	  			count = getSY.getInt(1);
+	  		}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return count;
+	}
+	
 	public int CountTransactionsSpecific(Connection conn, String studentid, String userid) {
 		int count = 0;
 		try {
